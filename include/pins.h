9872a1ddffeaaa6c09aced97bbfda5cf4ab0658a
VO QUANG VINH <vinh.vo28022005@gmail.com>
2026-06-11 09:17:33 +0700

    first commit

---
+++ include/pins.h
@@ -0,0 +1,29 @@
+#ifndef PINS_H
+#define PINS_H
+
+// Motor
+#define DIR_LEFT   5
+#define PWM_LEFT   6
+#define DIR_RIGHT  11
+#define PWM_RIGHT  10
+
+// IR sensor array (8 sensors)
+#define EN_SENSOR  8
+#define SENSOR_0   A0
+#define SENSOR_1   A1
+#define SENSOR_2   A2
+#define SENSOR_3   A3
+#define SENSOR_4   A4
+#define SENSOR_5   A5
+#define SENSOR_6   A6
+#define SENSOR_7   A7
+
+// RGB LED
+#define LED_RED    7
+#define LED_GREEN  12
+#define LED_BLUE   13
+
+// Fan
+#define PIN_FAN    9
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include "pins.h"
+#include "PID.h"
+#include "motor_control.h"
+#include "IR_sensor.h"
+#include "case_handling.h"
+
+void setup() {
+
+}
+
+void loop() {
+
+}
