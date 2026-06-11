4f2a677e96b85432238430efbe382108549741c2
VO QUANG VINH <vinh.vo28022005@gmail.com>
2026-06-11 09:31:02 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <Arduino.h>
 #include "pins.h"
+#include "IR_sensor.h"
 #include "PID.h"
 #include "motor_control.h"
-#include "IR_sensor.h"
 #include "case_handling.h"
 
 void setup() {
-
+    Serial.begin(9600);
+    IR_init();
+    IR_enable();
 }
 
 void loop() {
-
+    IR_read();
+    IR_print();
+    delay(200);
 }
